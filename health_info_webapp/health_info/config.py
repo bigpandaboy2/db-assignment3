c82479da509a84458995da9eb2abf3c71913a0af
Aibek Narik <aibek.narik8@gmail.com>
2024-11-18 22:25:16 +0500

    ainurka best

--- health_info_webapp/health_info/config.py
+++ health_info_webapp/health_info/config.py
@@ -1,9 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = (
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
         'postgresql://database_postgres_c915_user:OX1MeboNb9M13zKX05xm4upXXo4fsdYJ@dpg-cstm1mi3esus73d5e220-a/database_postgres_c915'
     )
-    print("Hello")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
